e66a467def524e16b07802b40caa9f0904b92764
AdrielBernhardT <adrielbth01@gmail.com>
2025-04-25 15:59:55 +0700

    Finished HCI Lab Project Web

--- Project Lab/AnotherPage/JS/DescReport.js
+++ Project Lab/AnotherPage/JS/DescReport.js
@@ -11,6 +11,16 @@ form.addEventListener('submit', function(event) {
         return;
     }
 
+    if(desc.value.length < 10){
+        alert("Description must be at least 10 characters long");
+        return;
+    }
+
+    if(desc.value.length > 1500){
+        alert("Description must be less than 1500 characters long");
+        return;
+    }
+
     form.reset();
     alert('Report submitted successfully');
     window.location.href = "../../../index.html";
--- Project Lab/AnotherPage/JS/Report.js
+++ Project Lab/AnotherPage/JS/Report.js
@@ -1,17 +1,34 @@
-let form = document.getElementById('container');
+const passwordInput = document.getElementById("Pass");
+const togglePassword = document.getElementById("togglePassword");
+
+togglePassword.addEventListener("click", () => {
+    const isPassword = passwordInput.type === "password";
+    passwordInput.type = isPassword ? "text" : "password";
+
+    togglePassword.src = isPassword 
+        ? "../../../Asset/REMIXICON/icons/System/eye-off-line.svg" 
+        : "../../../Asset/REMIXICON/icons/System/eye-line.svg";
+});
+
+const form = document.getElementById('container');
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    let username = document.getElementById("Username");
-    let password = document.getElementById("Pass");
+    const username = document.getElementById("Username");
+    const password = document.getElementById("Pass");
 
-    if(username.value == "" || password.value == ""){
+    if (username.value === "" || password.value === "") {
         alert("Please fill in all fields");
         return;
     }
 
-    if(password.value.length < 8){
+    if(username.value.length < 5) {
+        alert("Username must be at least 5 characters long");
+        return;
+    }
+
+    if (password.value.length < 8) {
         alert("Password must be at least 8 characters long");
         return;
     }
@@ -20,4 +37,4 @@ form.addEventListener('submit', function(event) {
     form.reset();
     window.location.href = "DescReport.html";
 });
-    
+
